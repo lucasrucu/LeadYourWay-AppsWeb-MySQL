c15a1a59382ab376291a3492773952655f1086d4
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-06-04 22:05:53 -0500

    first commit

---
+++ LeadYourWay.Domain/BicycleDomain.cs
@@ -0,0 +1,31 @@
+﻿using LeadYourWay.Infrastructure;
+using LeadYourWay.Infrastructure.Models;
+
+namespace LeadYourWay.Domain;
+
+public class BicycleDomain : IBicycleDomain
+{
+    public IBicycleInfrastructure _bicycleInfrastructure;
+    
+    public BicycleDomain(IBicycleInfrastructure bicycleInfrastructure)
+    {
+        _bicycleInfrastructure = bicycleInfrastructure;
+    }
+    
+    public bool save(Bicycle value)
+    {
+        // realizar validaciones aqui
+        return _bicycleInfrastructure.save(value);
+    }
+
+    public bool update(int id, Bicycle value)
+    {
+        // realizar validaciones aqui
+        return _bicycleInfrastructure.update(id, value);
+    }
+
+    public bool delete(int id)
+    {
+        return _bicycleInfrastructure.delete(id);
+    }
+}
